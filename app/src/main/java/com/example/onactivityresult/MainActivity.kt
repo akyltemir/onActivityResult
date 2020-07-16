82187e3ade722e8f5c82e7b4c4bf1afab1884ce9
akyltemir <67777557+akyltemir@users.noreply.github.com>
2020-07-16 15:44:28 -0500

    first commit

--- app/src/main/java/com/example/onactivityresult/MainActivity.kt
+++ app/src/main/java/com/example/onactivityresult/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.example.onactivityresult
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    var textView: TextView? = null
+    var textViewOne: TextView? = null
+    var textViewTwo: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
+        textViewOne = findViewById(R.id.textViewOne)
+        textViewTwo = findViewById(R.id.textViewTwo)
+
+
+        val buttonClick = findViewById<Button>(R.id.buttonClick)
+
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+
+            if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+                val forrest = data?.getStringExtra("forrest")
+                textView?.text = forrest
+
+                val legend = data?.getStringExtra("legend")
+                textViewOne?.text = legend
+
+                val fracture = data?.getStringExtra("fracture")
+                textViewTwo?.text = fracture
+            } else {
+                Log.d("onActivityResult", "no data")
+
+
+            }
+
+
+        }
+
+
     }
-}
+
+
+
+
+
+
+
+    }
--- app/src/main/java/com/example/onactivityresult/SecondActivity.kt
+++ app/src/main/java/com/example/onactivityresult/SecondActivity.kt
@@ -1,11 +1,68 @@
 package com.example.onactivityresult
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import kotlinx.android.synthetic.main.activity_main.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+
+        val checkBox = findViewById<CheckBox>(R.id.checkbox)
+        val checkboxOne = findViewById<CheckBox>(R.id.checkboxOne)
+        val checkboxTwo = findViewById<CheckBox>(R.id.checkboxTwo)
+        val buttonBack = findViewById<Button>(R.id.buttonBack)
+
+        var forrest: String? = null
+        var legend: String? = null
+        var fracture: String? = null
+
+        buttonBack.setOnClickListener {
+            val intent = Intent()
+
+            if (checkBox.isChecked) {
+                forrest = checkBox.text.toString()
+
+            }  else {
+                forrest = "Не выбрано"
+            }
+
+            if (checkboxOne.isChecked){
+                legend = checkboxOne.text.toString()
+            }
+            else{
+                legend = "Не выбрано"
+            }
+            if (checkboxTwo.isChecked){
+                fracture = checkboxTwo.text.toString()
+            }
+            else{
+                fracture = "Не выбрано"
+            }
+
+            intent.putExtra("forrest", forrest)
+            intent.putExtra("legend", legend)
+            intent.putExtra("fracture", fracture)
+
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+
+
+
+        }
+
+
+
+
+
+
+
+
     }
 }
